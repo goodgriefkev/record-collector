591e5b87f7f61369eb8d3d6f5ba904f53808d87c
Kevin Honnoll <khonnoll@gmail.com>
2019-06-02 17:25:13 -0400

    added index route for albums

--- controllers/albums.js
+++ controllers/albums.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 const Album = require('../models/albums.js');
 
 //INDEX
-
+router.get('/', (req, res) => {
+  res.send('index');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const albumsController = require('./controllers/albums.js');
+
+app.use('/albums', albumsController);
 
 app.listen(port, () => {
   console.log('listening on port', port);
