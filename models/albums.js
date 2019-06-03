2dac5acaf4be0ffd67761c3ea651d45eb2612618
Kevin Honnoll <khonnoll@gmail.com>
2019-06-02 20:08:07 -0400

    created Album model

--- models/albums.js
+++ models/albums.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose');
+
+const albumSchema = new mongoose.Schema({
+  title: {type:String, required:true},
+  artist: {type:String, required:true},
+  year: Number,
+  format: String,
+  description: String
+});
+
+const Album = mongoose.model('Album', albumSchema)
+
+module.exports = Album 
