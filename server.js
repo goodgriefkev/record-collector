efba834ca692e2cccd66e74101d71f62a6ab798c
Kevin Honnoll <khonnoll@gmail.com>
2019-06-08 12:22:24 -0400

    updated server file for heroku deployment

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const session = require('express-session');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const db = mongoose.connection;
 const port = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/recordsdb'
 const usersController = require('./controllers/users.js');
 const sessionsController = require('./controllers/sessions.js');
 const recordsController = require('./controllers/records.js');
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true});
+db.on('error', (err) => console.log(err.message + ' is Mondog not running'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+db.on('open', () => {});
 
 mongoose.connect('mongodb://localhost:27017/recordsdb', {
   useNewUrlParser: true
