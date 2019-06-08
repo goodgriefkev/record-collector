27d2870b0d6c7026d2a2b01dc30f7e0aca639a09
Kevin Honnoll <khonnoll@gmail.com>
2019-06-08 08:52:19 -0400

    added users controller and required it server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const port = process.env.PORT || 3000;
+const usersController = require('./controllers/users.js');
 const recordsController = require('./controllers/records.js');
 
 mongoose.connect('mongodb://localhost:27017/recordsdb', {
